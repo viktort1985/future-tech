b361c2f2952d27e1df4de3b472e527e40bb9472f
Victor Tarasov <myyoutoubelearning@gmail.com>
2025-10-26 00:36:21 +0300

    section video is complited

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import Header from './Header.js';
 import TabsCollection from './Tabs.js';
+import VideoPlayerCollection from './VideoPlayer.js';
 
 new Header();
-
+new VideoPlayerCollection();
 new TabsCollection();
